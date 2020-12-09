d40df8dde550b27a75ecc52868db614f67f719a7
t1h0n <34708388+t1h0n@users.noreply.github.com>
2020-12-09 18:14:10 +0100

    Update memento.cpp

--- memento.cpp
+++ memento.cpp
@@ -8,14 +8,12 @@ InsMemento::InsMemento(std::map<int, std::string> &Jmap, const jEntry &entry) : 
 
 std::shared_ptr<Memento> InsMemento::undo()
 {
-    std::cout<<"inserting: "<<entry.first<<"\n";
     Jmap.insert(entry);
     return std::make_shared<DelMemento>(Jmap, entry);
 }
 
 std::shared_ptr<Memento> DelMemento::undo()
 {
-    std::cout<<"deleting: "<<entry.first<<"\n";
     Jmap.erase(entry.first);
     return std::make_shared<InsMemento>(Jmap, entry);
 }
